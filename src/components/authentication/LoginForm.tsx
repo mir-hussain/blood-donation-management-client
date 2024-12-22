5053412b13fd4df2d712e49679a6e1c612fb286b
Mir Hussain <mirhussainmurtaza@gmail.com>
2024-12-22 20:12:08 +0600

    add: login form position

--- src/components/authentication/LoginForm.tsx
+++ src/components/authentication/LoginForm.tsx
@@ -14,8 +14,8 @@ export function LoginForm({
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card className="overflow-hidden">
         <CardContent className="grid p-0 md:grid-cols-2">
-          <form className="p-6 md:p-8">
-            <div className="flex flex-col gap-6">
+          <form className="w-full p-6 min-h-[500px] md:p-8">
+            <div className="flex justify-center flex-col gap-6 h-full">
               <div className="flex flex-col items-center text-center">
                 <h1 className="text-2xl font-bold">Welcome back</h1>
                 <p className="text-balance text-muted-foreground">
@@ -46,7 +46,7 @@ export function LoginForm({
               <Button type="submit" className="w-full">
                 Login
               </Button>
-              <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+              {/* <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
                 <span className="relative z-10 bg-background px-2 text-muted-foreground">
                   Or continue with
                 </span>
@@ -79,7 +79,7 @@ export function LoginForm({
                   </svg>
                   <span className="sr-only">Login with Meta</span>
                 </Button>
-              </div>
+              </div> */}
               <div className="text-center text-sm">
                 Don&apos;t have an account?{" "}
                 <Link to="/register" className="underline underline-offset-4">
