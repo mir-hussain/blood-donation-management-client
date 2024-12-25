189ec5991dbee531a4c2f301d5acf297736589dd
Mir Hussain <mirhussainmurtaza@gmail.com>
2024-12-25 12:32:37 +0600

    add: login

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,12 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/index.tsx";
 import { store } from "./redux/store";
+import { Toaster } from "sonner";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
+      <Toaster />
       <RouterProvider router={router} />
     </Provider>
   </StrictMode>
