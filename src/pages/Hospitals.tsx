a454c71c70fc4ed4185d1fcbb2e52240edce811a
Mir Hussain <mirhussainmurtaza@gmail.com>
2025-01-01 23:25:36 +0600

    add: dynamic contents from db

--- src/pages/Hospitals.tsx
+++ src/pages/Hospitals.tsx
@@ -1,12 +1,23 @@
 import HospitalCard from "@/components/modules/hospital/HospitalCard";
 import HospitalFilters from "@/components/modules/hospital/HospitalFilters";
 import Container from "@/components/ui/container";
+import Loading from "@/components/ui/loading";
+import { useGetHospitalsQuery } from "@/redux/features/hospital/hospitalApi";
+import { IHospital } from "@/types";
 
 export default function Hospitals() {
+  const { data, isLoading, isError } = useGetHospitalsQuery(undefined);
   return (
     <Container>
       <HospitalFilters />
-      <HospitalCard />
+      {isLoading && <Loading />}
+      {!isLoading && !isError && (
+        <div className="space-y-6">
+          {data?.data?.map((hospital: IHospital) => (
+            <HospitalCard hospital={hospital} />
+          ))}
+        </div>
+      )}
     </Container>
   );
 }
